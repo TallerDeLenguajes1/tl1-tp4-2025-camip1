c71edbe9c09bdd1fbfb33d9d55f80f629f4450ec
Camila Perez <camilap11517@gmail.com>
2025-04-22 11:45:31 -0300

    codigo distribuidora

--- distribuidora.c
+++ distribuidora.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 #include<time.h>
 #include<string.h>
-#define tareaInicial 1000;
+
+#define tareaInicial 1000
 
 struct Tarea{
     int TareaID;//Numérico autoincremental comenzando en 1000
@@ -17,53 +18,139 @@ struct Nodo{
     Nodo *Siguiente;
 }; 
 
-Nodo *ingresarTareas(Nodo *tareasPendientes);
-void mostrar(Nodo *tareasPendientes);
+Nodo * crearTarea(int i);
+void ingresarPendientes(Nodo **tareasPendientes);
+void mostrarTareas(Nodo *tareasPendientes, Nodo *tareasRealizadas);
+void registrarRealizadas(Nodo **tareasPendientes, Nodo **tareasRealizadas);
+void liberarLista(Nodo *lista);
 
 //Luego modularizar con el nombre To-Do
 int main(){
-    Nodo *tareasPendientes;
-    Nodo *tareasRealizadas;
-    //int cantPendientes;
-
-    ingresarTareas(tareasPendientes);
+    Nodo *tareasPendientes = NULL;
+    Nodo *tareasRealizadas = NULL;
+    ingresarPendientes(&tareasPendientes);
+    registrarRealizadas(&tareasPendientes,&tareasRealizadas);
+    mostrarTareas(tareasPendientes,tareasRealizadas);
     
-    /*tareasPendientes=(Nodo *)malloc(cantPendientes*sizeof(Nodo));
-    for (int i = 1; i <= cantPendientes; i++)
-    {
-
-    }*/
+    liberarLista(tareasPendientes);
+    liberarLista(tareasRealizadas);
     return 0;
 }
 
-Nodo *ingresarTareas(Nodo *tareasPendientes){
-    Nodo *nuevaTarea;
-    int cargar=1; //bandera para cargar tareas
-    int i=0; //contador para asignarle tamaño al puntero
-    char descripcion[50];
-    
-    while(cargar!=0){
-        Nodo *nuevaTarea=(Nodo *)malloc((i+1)*sizeof(Nodo));
-        
-        nuevaTarea->T.TareaID=i+tareaInicial; //muestra 1000 no suma;
-        printf("Ingresar descripcion de la tarea %d:", nuevaTarea->T.TareaID);
-        fflush(stdin);
-        gets(descripcion);
-        int tamCadena = strlen(descripcion);
-
-        nuevaTarea->T.Descripcion=(char *)malloc(tamCadena*sizeof(char) + 1);
-        strcpy(nuevaTarea->T.Descripcion,descripcion);  //copiar descripcion en el puntero descripcion
-        puts(nuevaTarea->T.Descripcion);
-
-        printf("ingrese la duracion de la tarea (en horas):");
-        scanf("%d",&nuevaTarea->T.Duracion);
-
-        nuevaTarea->Siguiente=tareasPendientes; //apunto nuevo nodo a la tarea anterior
-        tareasPendientes=nuevaTarea; //apunto start a la nueva tarea o nodo
-
-        printf("Desea cargar otra tarea? si(escribir 1)/no(escribir 0):");
-        scanf("%d",&cargar);
-        i++;
+void liberarLista(Nodo *lista) {
+    while (lista) {
+        Nodo *temp = lista;
+        lista = lista->Siguiente;
+        free(temp->T.Descripcion); // liberar descripción
+        free(temp); // liberar nodo
     }
-    return tareasPendientes;
 }
+
+Nodo * crearTarea(int i){
+    char descripcion[50];
+    Nodo *nuevaTarea=(Nodo *)malloc(sizeof(Nodo));
+    nuevaTarea->T.TareaID=i+tareaInicial; //muestra 1000 no suma;
+    
+    printf("Ingresar descripcion de la tarea %d:", nuevaTarea->T.TareaID);
+    getchar();
+    gets(descripcion);
+    int tamCadena = strlen(descripcion);
+    nuevaTarea->T.Descripcion=(char *)malloc(tamCadena*sizeof(char) + 1);
+    strcpy(nuevaTarea->T.Descripcion,descripcion);  //copiar descripcion en el puntero descripcion
+    
+    do {
+        printf("Ingrese la duracion de la tarea (entre 10 y 100): ");
+        scanf("%d", &nuevaTarea->T.Duracion);
+    } while(nuevaTarea->T.Duracion < 10 || nuevaTarea->T.Duracion > 100);
+    
+    nuevaTarea->Siguiente=NULL;
+    return nuevaTarea;
+}
+
+void ingresarPendientes(Nodo **tareasPendientes){
+    int cargar=1; //bandera para cargar tareas
+    int i=0; //contador para asignarle numero a la tarea
+    Nodo *nuevaTarea;
+    while(cargar==1){
+        nuevaTarea=crearTarea(i);
+        nuevaTarea->Siguiente = *tareasPendientes; //apunto nuevo nodo a la tarea anterior
+        *tareasPendientes = nuevaTarea; //apunto start a la nueva tarea o nodo
+        printf("\nDesea cargar otra tarea? si(escribir 1)/no(escribir 0):");
+        scanf("%d",&cargar);
+        i++; 
+    }
+}
+
+void registrarRealizadas(Nodo **tareasPendientes, Nodo **tareasRealizadas){
+    int registrar=1; //bandera para cargar tareas
+    int id; //para determinar la tarea a registrar
+    Nodo **aux = tareasPendientes;
+
+    while(registrar==1){
+        printf("\ningrese el id de la tarea realizada:");
+        scanf("%d",&id);
+
+        aux = tareasPendientes;
+
+        while (*aux!=NULL && (*aux)->T.TareaID!=id)
+        {
+            aux=&(*aux)->Siguiente;
+        }
+
+        if (*aux!=NULL)
+        {
+            Nodo *realizada=*aux;
+            *aux=(*aux)->Siguiente;
+
+            realizada->Siguiente=  *tareasRealizadas;
+            *tareasRealizadas = realizada;
+            
+            printf("Tarea completada registrada.\n");
+        }
+        else {
+            printf("ID invalido. Operacion cancelada.\n");
+        }
+
+        printf("\nDesea registrar otra tarea realizada? si(escribir 1)/no(escribir 0):");
+        scanf("%d",&registrar);
+    }
+}
+
+
+void mostrarTareas(Nodo *tareasPendientes, Nodo *tareasRealizadas){
+    Nodo *aux;
+    printf("\n\nTareas pendientes");
+    if (tareasPendientes!=NULL){
+        aux=tareasPendientes;
+        while (aux!=NULL)
+        {
+            printf("\ntarea %d: %s\n", aux->T.TareaID, aux->T.Descripcion);
+            printf("Duracion: %d", aux->T.Duracion);
+            aux=aux->Siguiente;
+        }
+    }
+    else{
+        printf("\nVacia");
+    }
+    printf("\n\nTareas completadas");
+    if (tareasRealizadas!=NULL)
+    {
+        aux=tareasRealizadas;
+        while(aux!=NULL)
+        {
+            printf("\ntarea %d: %s\n",aux->T.TareaID, aux->T.Descripcion);
+            printf("Duracion: %d",aux->T.Duracion);
+            aux=aux->Siguiente;
+        }
+    }
+    else{
+        printf("\nVacia");
+    }
+}
+
+/*
+2)Implemente una interfaz para elegir qué tareas de la lista de pendientes deben ser
+transferidas a la lista de tareas realizadas.
+3) Implemente una funcionalidad que permita listar todas las tareas pendientes y realizadas.
+4) Implemente una funcionalidad que permita consultar tareas por id o palabra clave y
+mostrarlas por pantalla, indicando si corresponde a una tarea pendiente o realizada*/
